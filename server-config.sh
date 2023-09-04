625978ab3e4fb277e4f908db76db419ec0fd3c7c
MudassirKhan22 <khanmudassir2000@gmail.com>
2023-09-04 18:01:53 +0530

    Updating Jenkinsfile

--- server-config.sh
+++ server-config.sh
@@ -6,6 +6,7 @@ sudo yum install maven -y
 if [ -d "addressbookpractice" ]
 then
     echo "Repository is already cloned and exist"
+    cd /home/ec2-user/addressbookpractice
     git pull origin Practice
 else
 git clone https://github.com/MudassirKhan22/addressbookpractice.git
